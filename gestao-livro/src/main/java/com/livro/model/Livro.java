05fb228139c69147a3b9f1d1dc2e52a19058f7ca
adrianomoda <adrianomodafeitosa@gmail.com>
2021-01-17 11:03:00 -0300

    Projeto com Lombok

--- gestao-livro/src/main/java/com/livro/model/Livro.java
+++ gestao-livro/src/main/java/com/livro/model/Livro.java
@@ -2,105 +2,38 @@ package com.livro.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Table(name = "livro")
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(of = "id") @ToString
 public class Livro implements Serializable {
 	
 	private static final long serialVersionUID = 6865151781535323830L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+	@Getter @Setter private Long id;	
+	@Getter @Setter private String nome;	
+	@Getter @Setter private String serie;	
+	@Getter @Setter private boolean seEmprestado;
+	@JsonFormat(shape = Shape.STRING, pattern = "dd-MM-yyyy' 'HH:mm")
+	@Getter @Setter private Date registroEmprestimo;	
 	
-	@Column
-	private String nome;
-	
-	@Column
-	private String serie;
-	
-	@Column
-	private boolean emprestimo;
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date registroEmprestimo;
-	
-	/* Getters e Setters */
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getSerie() {
-		return serie;
-	}
-
-	public void setSerie(String serie) {
-		this.serie = serie;
-	}
-
-	public boolean isEmprestimo() {
-		return emprestimo;
-	}
-
-	public void setEmprestimo(boolean emprestimo) {
-		this.emprestimo = emprestimo;
-	}
-
-	public Date getRegistroEmprestimo() {
-		return registroEmprestimo;
-	}
-
-	public void setRegistroEmprestimo(Date registroEmprestimo) {
-		this.registroEmprestimo = registroEmprestimo;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Livro other = (Livro) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Livro [id=" + id + ", nome=" + nome + ", serie=" + serie + ", emprestimo=" + emprestimo
-				+ ", registroEmprestimo=" + registroEmprestimo + "]";
-	}
 }
